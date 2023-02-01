3170dae5690751676df52b14c00c2a5fdb173fa2
Sahadeb Barman <saha.ptc.om@gmail.com>
2023-02-01 23:12:12 +0000

    Serving Html with node js

--- Node.js : Writing Our First Server/index.js
+++ Node.js : Writing Our First Server/index.js
@@ -3,7 +3,13 @@ const http = require('http');
 const port=8000;
 function requestHandler(req,res) {
     console.log(req.url);
-    res.end('Hi, There...... It Works')
+   // res.end('Hi, There...... It Works')
+
+   res.writeHead(200,{
+    'content-type': 'text/html'
+   });
+   res.end(' <h1>Hello! Serving HTML Throgh Node Js</h1>')
+  
     
 }
 const server = http.createServer(requestHandler);
