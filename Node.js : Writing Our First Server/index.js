e7a7ad7476e71eb1b0f9d77db7a634d24f27752f
Sahadeb Barman <saha.ptc.om@gmail.com>
2023-02-01 23:48:14 +0000

    Handling Multiple files  with Node Js

--- Node.js : Writing Our First Server/index.js
+++ Node.js : Writing Our First Server/index.js
@@ -19,11 +19,42 @@ function requestHandler(req,res) {
 */
 
 // Handling File with node js
+/**
 
 function requestHandler(req,res){
     console.log((req.url));
     res.writeHead(200,{'content-type': 'text/html'});
     fs.readFile('./index.html', function(err,data){
+        if(err){
+            console.log('error',err);
+            return res.end('<h1>Error!</h1>');
+        }
+
+        return res.end(data);
+    })
+}
+*/
+
+function requestHandler(req,res){
+    console.log((req.url));
+    res.writeHead(200,{'content-type': 'text/html'});
+
+    let fileePath ;
+    switch (req.url) {
+        case '/':
+            fileePath='./index.html'
+            break;
+        case '/profile':
+            fileePath='./profile.html'
+            break;
+    
+        default:
+            fileePath='./404.html'
+            break;
+    }
+
+
+    fs.readFile(fileePath, function(err,data){
         if(err){
             console.log('error',err);
             return res.end('<h1>Error!</h1>');
